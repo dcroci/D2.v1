dae0503fc0f5a2c39430154108939e6ce39ed559
Dominik Croci <dominikrcroci@gmail.com>
2023-07-06 12:05:32 -0700

    planet game implemented

--- planet.js
+++ planet.js
@@ -1,6 +1,55 @@
 const questionImg = document.querySelector('.questionImg');
-const body = document.querySelector('body');
+const submit = document.querySelector('#submit');
+let guessCount = 0;
 
+const locations = {
+  'Black Garden': [
+    'assets/locations/black-garden/destiny-black-garden-campaign-black-garden.jpg',
+    'assets/locations/black-garden/zu4puo78v3j51.webp',
+  ],
+  Farm: ['assets/locations/farm/destiny-the-farm.avif'],
+  Lighthouse: [
+    'assets/locations/lighthouse/destiny-2-trials-of-osiris-lighthouse-loot-1.avif',
+    'assets/locations/lighthouse/destiny-2-trials-of-osiris.jpg',
+  ],
+  Menagerie: [
+    'assets/locations/menagerie/all-menagerie-triumphs-destiny-2_feature.jpg',
+  ],
+  Tower: [
+    'assets/locations/tower/Destiny_2_Tower_Courtyard.webp',
+    'assets/locations/tower/destiny-2-revelry-tower-credit-bungie@2000x1270-1.jpg',
+  ],
+  'Twilight Gap': [
+    'assets/locations/twilight-gap/c999282e1a62b2aea72e527e45e24b85.jpg',
+  ],
+};
+const locationNames = Object.keys(locations);
+const randomLocationName =
+  locationNames[Math.floor(Math.random() * locationNames.length)];
+const locationImages = locations[randomLocationName];
+const randomImageUrl =
+  locationImages[Math.floor(Math.random() * locationImages.length)];
+questionImg.src = randomImageUrl;
+function raiseCount() {
+  guessCount++;
+  console.log(guessCount);
+}
+function checkGuess() {
+  const guess = document.getElementById('guess').value;
+  const resultEl = document.getElementById('result');
+  if (guess === '' || guess === ' ') {
+    alert('Please enter a guess.');
+  } else if (guess.toLowerCase() === randomLocationName.toLowerCase()) {
+    resultEl.textContent = 'Correct!';
+    questionImg.style.transform = 'scale(1)';
+  } else {
+    resultEl.textContent = 'Incorrect. Try again.';
+  }
+}
+function guessing() {
+  raiseCount();
+  checkGuess();
+}
 function zoomOut() {
   const computedStyle = window.getComputedStyle(questionImg);
   const currentScale = parseFloat(computedStyle.transform.split(' ')[3]);
@@ -10,6 +59,8 @@ function zoomOut() {
   }
 }
 
-questionImg.addEventListener('click', () => {
-  zoomOut();
+submit.addEventListener('click', () => {
+  if (guess.innerHTML !== randomLocationName) {
+    zoomOut();
+  }
 });
